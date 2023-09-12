f903aedc7ad0cfa0f6adb8c29a5e865be0413ce2
Iris Marechal <irismarechal@gmail.com>
2023-09-12 15:25:36 +0200

    test 2

--- interface/maindataset.py
+++ interface/maindataset.py
@@ -6,4 +6,11 @@ data_2 = pd.read_csv('raw_data/230911_Hate_Speech_and_Offensive_Language_01.csv'
 hatespeech = pd.read_csv('raw_data/230911_Dynamically_Generated_Hate_Speech_01.csv')
 hateXplain = pd.read_json('raw_data/230911_HateXplain.json')
 
-cleaning_data(data_1,data_2,hatespeech,hateXplain)
+parent=True #si notebook false
+#Download
+if parent==True:
+    target_path='data/clean_dataset_v3.csv'
+else:
+    target_path='../data/clean_dataset_v3.csv'
+
+cleaning_data(data_1,data_2,hatespeech,hateXplain).to_csv(target_path, index=False)
