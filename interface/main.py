f2e1e6a219499ec2ca73db055f60b5e46c786b5a
Lua Destailleurs <luadestailleurs@gmail.com>
2023-09-14 14:33:22 +0200

    Final

--- interface/main.py
+++ interface/main.py
@@ -1,10 +1,9 @@
 
 ## Import
-
-from utils.ml_baseline import baseline_model
 import pandas as pd
 from utils.registry import save_model, load_model
-from utils.dl import GRU_model, LSTM_model, Conv1D_modelGo
+from utils.ml_baseline import baseline_model
+from utils.dl import GRU_model, LSTM_model, Conv1D_model
 
 ### Baseline_model
 
@@ -41,7 +40,7 @@ def pred_baseline(X_pred: str = None) -> str:
 
 ### Train DeepL models
 
-def train_DL_model(processed=False, model_name): ### select Conv1D, or GRU, or LSTM
+def train_DL_model(model_name, processed=False): ### select Conv1D, or GRU, or LSTM
 
     if model_name == "Conv1D":
         if processed:
@@ -71,7 +70,7 @@ def train_DL_model(processed=False, model_name): ### select Conv1D, or GRU, or L
 
 ### Predict with  DeepL models
 
-def pred_DL(X_pred: str = None, model_name) -> str:
+def pred_DL(model_name, X_pred: str = None) -> str:
     """
     Make a prediction using the latest trained model
     """
@@ -97,6 +96,7 @@ def pred_DL(X_pred: str = None, model_name) -> str:
 if __name__ == "__main__":
     while True:
         X_pred = str(input("Enter a tweet: "))
+        model_name = str(input("Enter model name between LSTM, GRU and Conv1D : "))
         pred_baseline(X_pred)
-        train_DL_model(processed=False, model_name)
-        pred_DL(X_pred: str = None, model_name)
+        train_DL_model(model_name, processed=False)
+        pred_DL(model_name, X_pred)
